93415522e0b8df0fc1efbdcd28188ad6bcae09cd
Sabino Fernandez <sabinofernandezr@gmail.com>
2022-08-18 23:33:00 -0500

    added unique id to quotes

--- routes/quotes.routes.js
+++ routes/quotes.routes.js
@@ -1,4 +1,5 @@
 const { Router, json } = require('express');
+const { nanoid } = require('nanoid');
 
 const quoteService = require('../services/quotes.service');
 
@@ -7,40 +8,80 @@ const quotesRouter = Router();
 quotesRouter.use(json());
 
 quotesRouter.get('/', getQuote);
-// quotesRouter.get('/:amount', getQuotes);
-quotesRouter.get('/character/:character', getQuoteByCharacter);
+quotesRouter.get('/search', getQuotesByCharacter);
+quotesRouter.get('/character', getQuotesByCharacter);
 
 async function getQuote(req, res, next) {
 	try {
 		const quotes = await quoteService.getQuote();
-		res.json(quotes);
+		const { quote, character, image, characterDirection } = quotes;
+		const data = {
+			quote,
+			character,
+			image,
+			characterDirection,
+			id: nanoid(8),
+		};
+		res.json([data]);
 	} catch (error) {
 		next(error);
 	}
 }
 
-/* async function getQuotes(req, res) {
-	const { amount } = req.params;
-	console.log('amount: ', amount);
+async function getQuotesByCharacter(req, res, next) {
+	const { amount, name } = req.query;
+
 	try {
-		const response = await axios.get(
-			`https://thesimpsonsquoteapi.glitch.me/quotes?count=${amount}`
-		);
-		res.json(response.data);
+		const response = await quoteService.getQuotesByCharacter(name, amount);
+
+		if (amount) {
+			const data = [];
+			response.map((res) => {
+				const { quote, character, image, characterDirection } = res;
+				data.push({
+					quote,
+					character,
+					image,
+					characterDirection,
+					id: nanoid(8),
+				});
+			});
+			return res.json(data);
+		}
+
+		const { quote, character, image, characterDirection } = response[0];
+		const data = {
+			quote,
+			character,
+			image,
+			characterDirection,
+			id: nanoid(8),
+		};
+
+		res.json([data]);
 	} catch (error) {
-		console.error(error);
+		next(error);
+	}
+}
+
+/* async function getQuoteByCharacter(req, res, next) {
+	try {
+		const { name } = req.params;
+		const quotesBy = await quoteService.getQuotesByCharacter(name);
+
+		const { quote, character, image, characterDirection } = quotesBy;
+		const data = {
+			quote,
+			character,
+			image,
+			characterDirection,
+			id: nanoid(8),
+		};
+
+		res.json([data]);
+	} catch (error) {
+		next(error);
 	}
 } */
-
-async function getQuoteByCharacter(req, res, next) {
-	try {
-		const { character } = req.params;
-
-		const quotesBy = await quoteService.getQuoteByCharacther(character);
-		res.json(quotesBy);
-	} catch (error) {
-		next(error);
-	}
-}
 
 module.exports = quotesRouter;
--- services/quotes.service.js
+++ services/quotes.service.js
@@ -6,13 +6,24 @@ class QuotesService {
 		const response = await axios.get(
 			'https://thesimpsonsquoteapi.glitch.me/quotes'
 		);
-		return response.data;
+		return response.data[0];
 	}
 
 	async getQuoteByCharacther(character) {
 		const response = await axios.get(
 			`https://thesimpsonsquoteapi.glitch.me/quotes?character=${character}`
 		);
+		if (response.data.length === 0) {
+			throw boom.notFound('There are no quotes');
+		}
+		return response.data[0];
+	}
+
+	async getQuotesByCharacter(character, amount = 1) {
+		const response = await axios.get(
+			`https://thesimpsonsquoteapi.glitch.me/quotes?count=${amount}&character=${character}`
+		);
+
 		if (response.data.length === 0) {
 			throw boom.notFound('There are no quotes');
 		}
